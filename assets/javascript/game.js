51d78aca47d2e32ce8e2dbbc5e5dcdc6af07d8b7
Ken Dacanay <ken.p.r.dacanay@gmail.com>
2020-03-05 22:40:00 -0500

    wrote functions for crystal game, able to click buttons to up score, logic there except for losses, unable to update losses total for some reason.  But now I am sleepy

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,10 +2,10 @@
 //<div id="winningScore">Score to Match:</div>
 //<div id="winsTotal">Wins:</p>
 //<div id="lossesTotal">Losses:</p>
-//<div id="red-button">Ruby</div>
-//<div id="blue-button">Diamond</div>
-//<div id="yellow-button">Sapphire</div>
-//<div id="green-button">Emerald</div>
+//<div id="red-crystal">Ruby</div>
+//<div id="blue-crystal">Diamond</div>
+//<div id="yellow-crystal">Sapphire</div>
+//<div id="green-crystal">Emerald</div>
 //<div id="totalScore">Your Total Score Is:</div>
 //<div id="scoreDisplay">" "</div>
 
@@ -23,7 +23,6 @@ var winsTotal = 0;
 var lossesTotal = 0;
 var winningScore = 0;
 var totalScore = 0;
-var scoreDisplay = " ";
 var rubyPoints = " ";
 var diamondPoints = " ";
 var sapphirePoints = " ";
@@ -34,23 +33,76 @@ startGame();
 
 
 //function to generate winningScore 
-function generateWinningScore(min,max) {
-     return Math.floor(Math.random() * (max-min) + min);
+function generateWinningScore(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
 }
 //function to display winningScore
-function displayWinningScore() {
-    document.getElementById("winningScore").innerHTML = "Score to Match: " + winningScore;    
+function updateWinningScore() {
+    document.getElementById("winningScore").innerHTML = "Score to Match: " + winningScore;
 }
-//function to display current player score (totalScore)
+//function to display current player score (scoreDisplay)
 function updateTotalScore() {
-    document.getElementById("scoreDisplay").innerHTML = " " + totalScore;
+    document.getElementById("totalScore").innerHTML = " " + totalScore;
 }
-
+//function to display winsTotal
+function updateWinsTotal() {
+    document.getElementById("winsTotal").innerHTML = "Wins: " + winsTotal;
+}
+//function to display lossesTotal
+function updateLossesTotal() {
+    document.getElementById("lossesTotal").innerHtml = "Losses: " + lossesTotal;
+}
+function compare() {
+    updateTotalScore();
+    if (totalScore === winningScore) {
+        winsTotal++;
+        updateWinsTotal();
+        startGame();
+    }
+    if (totalScore > winningScore) {
+        lossesTotal++;
+        updateLossesTotal();
+        startGame();
+    }
+}
 //function for starting game: display random winningScore, sets all player totals to 0, chooses
-//random points for gems to be added to totalScore
+//random points for gems to be added to scoreDisplay
 function startGame() {
     winningScore = generateWinningScore(75, 150);
-    displayWinningScore();
+    updateWinningScore();
     updateTotalScore();
+    updateWinsTotal();
+    updateLossesTotal();
+    crystalPoints(1, 12);
+    totalScore = 0;
+}
+
+//functions to set up values of crystals, random values from 1 to 12 to each crystal
+function crystalPoints(min, max) {
+    rubyPoints = Math.floor(Math.random() * (max - min + 1) + min);
+    diamondPoints = Math.floor(Math.random() * (max - min + 1) + min);
+    sapphirePoints = Math.floor(Math.random() * (max - min + 1) + min);
+    emeraldPoints = Math.floor(Math.random() * (max - min + 1) + min);
+    console.log("Ruby: " + rubyPoints + ", Diamond: " + diamondPoints + ", Sapphire: " + sapphirePoints + ", Emerald: " + emeraldPoints);
+}
+
+function rubyButton() {
+    totalScore = totalScore + rubyPoints;
+    compare();
+}
+
+function diamondButton() {
+    totalScore = totalScore + diamondPoints;
+    compare();
+}
+
+function sapphireButton() {
+    totalScore = totalScore + sapphirePoints;
+    compare();
+}
+
+function emeraldButton() {
+    totalScore = totalScore + emeraldPoints;
+    compare();
 }
 
